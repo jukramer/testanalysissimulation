b5b1448f68d3c75d0b58eb14239b6223a85f7a49
timmmbl <tim.mail@xs4all.nl>
2026-03-10 17:44:50 +0100

    final updating done with project

--- Test file.py
+++ Test file.py
@@ -4,14 +4,19 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
-sdf, sdf_sinks = sarracen.read_phantom('incl_30/incl_30_00000')
+sdf, sdf_sinks = sarracen.read_phantom('retrograde/retrograde_00007')
 
 
 sdf.calc_density()
 # sdf.describe()
-# print(sdf)
+print(sdf)
 print(sdf_sinks)
 # print(sdf['itype'].value_counts())
+
+# For 30 degrees incline it seems the x, y and z are kind of swapped or something
+
+
+
 
 # ower_bound = df['column_name'].quantile(0.05) and upper_bound = df['column_name'].quantile(0.95)
 
@@ -22,9 +27,11 @@ y1 = sdf['y'].quantile(0.05)
 y2 = sdf['y'].quantile(0.95)
 
 # only getting data from protoplanetary disc sink
-x_sink_1 = sdf_sinks.at[0, 'x']
-y_sink_1 = sdf_sinks.at[0, 'y']
+x_sink_0 = sdf_sinks.at[0, 'x']
+y_sink_0 = sdf_sinks.at[0, 'y']
 
-ax = sdf[sdf.itype == 1].render('rho', xlim=(x1 -100, x2 + 100), ylim=(y1 -100, y2 + 100), log_scale=True, xsec=0.0)
-ax.scatter(x=x_sink_1, y=y_sink_1, color='white')
+
+
+ax = sdf[sdf.itype == 1].render('rho', xlim=(x1 -100, x2 + 100), ylim=(y1 -100, y2 + 100), log_scale=True, xsec=0.00)
+ax.scatter(x=x_sink_0, y=y_sink_0, color='white')
 plt.show()
