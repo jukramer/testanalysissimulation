85a23e8e8a9d47d427904aeee90dc0a3e1c91209
jukramer <justas.kramer@gmail.com>
2026-03-10 15:10:14 +0100

    updates

--- calc.py
+++ calc.py
@@ -3,3 +3,9 @@ import numpy as np
 import pandas as pd
 import sarracen as sc
 
+def fun():
+    pass
+
+if __name__ == '__main__':
+    pass
+
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import numpy as np
+import pandas as pd
+
+
 if __name__ == '__main__':
-    pass
+    data = np.fromfile("")
+    
