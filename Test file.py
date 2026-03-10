b95ed86d394b8dbd70efeb4dcf66dc47c4b6d7a3
timmmbl <tim.mail@xs4all.nl>
2026-03-10 16:58:11 +0100

    updates

--- Test file.py
+++ Test file.py
@@ -1,5 +1,6 @@
 import sarracen
 import pandas
+import matplotlib
 
 
 sdf, sdf_sinks = sarracen.read_phantom('prograde/prograde_00000')
@@ -8,7 +9,7 @@ sdf, sdf_sinks = sarracen.read_phantom('prograde/prograde_00000')
 sdf.calc_density()
 # sdf.describe()
 print(sdf)
-# print(sdf_sinks)
+print(sdf_sinks)
 # print(sdf['itype'].value_counts())
 
 # sdf[sdf.itype == 1].render('rho', xlim=(-1000, 1000), ylim=(-1000, 1000), log_scale=True, xsec=0.0)
