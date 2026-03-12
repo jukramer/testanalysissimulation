d6d5a679c4a746987a532d5b0b4bc2eaf5b9afcb
timmmbl <tim.mail@xs4all.nl>
2026-03-12 09:03:34 +0100

    tried some centering of plots

--- Test file.py
+++ Test file.py
@@ -3,7 +3,7 @@ import pandas
 import matplotlib.pyplot as plt
 
 
-sdf, sdf_sinks = sarracen.read_phantom('prograde/prograde_00010')
+sdf, sdf_sinks = sarracen.read_phantom('prograde/prograde_00011')
 
 
 sdf.calc_density()
@@ -27,10 +27,12 @@ y_sink_1 = sdf_sinks.at[1, 'y']
 
 plt.style.use('dark_background')
 
-ax = sdf[sdf.itype == 1].render('rho', xlim=(x1 -100, x2 + 100), ylim=(y1 -100, y2 + 100), log_scale=True, xsec=0.00)
+ax = sdf[sdf.itype == 1].render('rho', xlim=(x_sink_0 - 500, x_sink_0 + 500), ylim=(y_sink_0 - 500, y_sink_0 + 500), log_scale=True, xsec=0.00)
 ax.scatter(x=x_sink_0, y=y_sink_0, color='white')
 ax.scatter(x=x_sink_1, y=y_sink_1, color='white')
 
-# For next session see if it is possible to have the heatmap be blue
+# TODO See if it is possible to have the heatmap be blue
+# TODO Centering follows 95th percentile rule
+# TODO Work on radial binning analysis
 
 plt.show()
