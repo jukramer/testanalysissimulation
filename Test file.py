ba088ddfdc2080316c4e5349eca3b56c867078e2
zorkafischer <zorka@dokiten.io>
2026-03-17 14:36:03 +0100

    added comments to describe code

--- Test file.py
+++ Test file.py
@@ -24,10 +24,10 @@ sdf_sinks.at[0, 'y'] = sdf_sinks.at[0, 'y'] - sdf_sinks.at[0, 'y']
 
 #Creating dots for sink particles
 
-
+# star sink particle
 x_sink_0 = sdf_sinks.at[0, 'x'] 
 y_sink_0 = sdf_sinks.at[0, 'y'] 
-
+# flyby sink particle
 x_sink_1 = sdf_sinks.at[1, 'x']
 y_sink_1 = sdf_sinks.at[1, 'y']
 
@@ -41,8 +41,14 @@ plt.style.use('dark_background')
 # ax = sdf[sdf.itype == 1].render('rho', xlim=(x_sink_0 - 700, x_sink_0 + 700), ylim=(y_sink_0 - 700, y_sink_0 + 700), log_scale=True, xsec=0.00)
 # ax = sdf[sdf.itype == 1].render('rho', xlim=(x_sink_0 - 700, x_sink_0 + 700), ylim=(y_sink_0 - 700, y_sink_0 + 700), log_scale=True)
 
+# for sdf.itype, 1 = gas, 7 = dust species A, 8 = dust species B
+
+# column integrated view
 # ax = sdf[sdf.itype == 1].render('rho', xlim=(- 400,  400), ylim=(-400, 400), log_scale=True)
+
+# sectional view at z = 0
 ax = sdf[sdf.itype == 1].render('rho', xlim=(- 400,  400), ylim=(-400, 400), log_scale=True, xsec=0.00)
+
 
 # Sink particles visualisation
 ax.scatter(x=x_sink_0, y=y_sink_0, color='white')
