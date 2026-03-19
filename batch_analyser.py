615f7310d185c4bdc23fbdfaafdfbee94146b8de
Lance <lance1220du@icloud.com>
2026-03-19 09:45:10 +0100

    batch analyser

--- batch_analyser.py
+++ batch_analyser.py
@@ -1,14 +1,15 @@
-import glob, os
+import os
 from render import render
 
+OUTPUT_PATH = "Renders"
 FOLDERS = ['incl_30', 'prograde', 'retrograde']
 
 # Chek if output folders exist, make dir if non existent
-if not os.path.isdir("Renders"):
-    os.mkdir("Renders")
-    for folder in FOLDERS:
-        if not os.path.exists("Renders/"+folder):
-            os.mkdir("Renders/"+folder)
+if not os.path.isdir(OUTPUT_PATH):
+    os.mkdir(OUTPUT_PATH)
+for folder in FOLDERS:
+    if not os.path.exists(f"{OUTPUT_PATH}{folder}"):
+        os.mkdir(f"{OUTPUT_PATH}{folder}")
 
 
 #loop through input folder, find files starting with correct filename, and output to correct location
@@ -16,7 +17,7 @@ for folder in FOLDERS:
     for file in os.listdir(folder):
         if file.startswith(f"{folder}_"):
             print(f"Processing {file} from {folder}")
-            output = render(file)
-            output.savefig(f"Renders/{folder}/{file}")
+            # output = render(file)
+            # output.savefig(f"Renders/{folder}/{file}")
 
 
