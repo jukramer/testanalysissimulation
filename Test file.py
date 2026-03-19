cc2f34d8179c9debcab38742b85e260ac92a641d
zorkafischer <zorka@dokiten.io>
2026-03-19 12:10:15 +0100

    found good colour combo

--- Test file.py
+++ Test file.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import sarracen
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
+from matplotlib.colors import LogNorm
 
-SECTIONAL_VIEW = False
+
+
+SECTIONAL_VIEW = True
 
 sdf, sdf_sinks = sarracen.read_phantom('prograde/prograde_00010')
 
 
 sdf.calc_density()
+
 # sdf.describe()
 print(sdf)
 print(sdf_sinks)
@@ -46,6 +52,14 @@ def plot_sinks(ax):
     ax.scatter(x=x_sink_0, y=y_sink_0, color='white')
     ax.scatter(x=x_sink_1, y=y_sink_1, color='white')
 
+# truncating cmap
+def truncate_cmap(cmap_name, minval=0.2, maxval=1.0, n=256):
+    cmap = plt.get_cmap(cmap_name)
+    return mcolors.LinearSegmentedColormap.from_list(
+        f"trunc_{cmap_name}",
+        cmap(np.linspace(minval, maxval, n))
+    )
+
 # sectional view at z = 0 , for sdf.itype, 1 = gas, 7 = dust (stokes = 10), 8 = dust (stokes = 1)
 
 if SECTIONAL_VIEW:
@@ -56,9 +70,9 @@ if SECTIONAL_VIEW:
     plt.show()
 
     ax_2 = sdf[sdf.itype == 7].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True, xsec=0.00,
-                                      cmap='gray', alpha=0.6)
-    sdf[sdf.itype == 8].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True, xsec=0.00, cmap='gist_heat',
-                               ax=ax_2, alpha=0.8)
+                                      cmap= truncate_cmap('bone',0.1,1), alpha=1)
+    sdf[sdf.itype == 8].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True, cmap= truncate_cmap('Blues',0.3,1),
+                               ax=ax_2, alpha=0.4)
     ax_2.set_title("Dust Distribution in Disc")
     plot_sinks(ax_2)
     plt.show()
@@ -70,9 +84,10 @@ else:
     plt.show()
 
     ax_2 = sdf[sdf.itype == 7].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True,
-                                      cmap='gray', alpha=0.6)
-    sdf[sdf.itype == 8].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True, cmap='gist_heat',
-                               ax=ax_2, alpha=0.8)
+                                      cmap=truncate_cmap('bone', 0.1, 1), alpha=1)
+    sdf[sdf.itype == 8].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True,
+                               cmap=truncate_cmap('Blues', 0.3, 1),
+                               ax=ax_2, alpha=0.4)
     ax_2.set_title("Dust Distribution in Disc")
     plot_sinks(ax_2)
     plt.show()
