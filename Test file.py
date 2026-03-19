f396904af5e3986f800bb6bca2fb5fe1f5722a92
zorkafischer <zorka@dokiten.io>
2026-03-19 12:32:01 +0100

    trying to make background black but failing

--- Test file.py
+++ Test file.py
@@ -52,14 +52,19 @@ def plot_sinks(ax):
     ax.scatter(x=x_sink_0, y=y_sink_0, color='white')
     ax.scatter(x=x_sink_1, y=y_sink_1, color='white')
 
-# truncating cmap
+#
+v_min = 10**(-10)
+# function to truncated colour maps
 def truncate_cmap(cmap_name, minval=0.2, maxval=1.0, n=256):
     cmap = plt.get_cmap(cmap_name)
-    return mcolors.LinearSegmentedColormap.from_list(
+    new_cmap = mcolors.LinearSegmentedColormap.from_list(
         f"trunc_{cmap_name}",
-        cmap(np.linspace(minval, maxval, n))
-    )
+        cmap(np.linspace(minval, maxval, n)))
+    new_cmap.set_under(color='black')
+    return new_cmap
 
+cmap_dust = plt.get_cmap('Blues').copy()
+cmap_dust.set_under('black')
 # sectional view at z = 0 , for sdf.itype, 1 = gas, 7 = dust (stokes = 10), 8 = dust (stokes = 1)
 
 if SECTIONAL_VIEW:
@@ -71,7 +76,7 @@ if SECTIONAL_VIEW:
 
     ax_2 = sdf[sdf.itype == 7].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True, xsec=0.00,
                                       cmap= truncate_cmap('bone',0.1,1), alpha=1)
-    sdf[sdf.itype == 8].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True, cmap= truncate_cmap('Blues',0.3,1),
+    sdf[sdf.itype == 8].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True, cmap = cmap_dust, vmin=v_min,
                                ax=ax_2, alpha=0.4)
     ax_2.set_title("Dust Distribution in Disc")
     plot_sinks(ax_2)
