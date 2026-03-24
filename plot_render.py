3c6a3a2982971d135904c80350270ee14e2138aa
zorkafischer <zorka@dokiten.io>
2026-03-24 17:20:24 +0100

    continued plot render

--- plot_render.py
+++ plot_render.py
@@ -1,22 +1,27 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
-from Test_file import *
+from Test_file import cmap_dust, sdf_creator, subplot_gas, subplot_dust1, subplot_dust2, plot_sinks
 
-n_rows = 12
+n_rows = 5
 n_cols = 3
 fig, ax  = plt.subplots(nrows= n_rows, ncols= n_cols)
-
+plt.style.use('dark_background')
 mappable = None
 
 # for gas distribution
 cmap = cmap_dust
 norm = LogNorm()
+encounter = ['prograde','retrograde','incl_30']
 for i in range(n_rows):
     for j in range(n_cols):
+        ax = ax[i, j]
         if i < 10:
-            ax[i,j] = subplot_gas(f'prograde_0000{i}')
+            sdf, sdf_sinks = sdf_creator(f'{encounter[j]}_0000{i}')
+            mappable = subplot_gas(sdf, True)
         else:
-            ax[i,j] = subplot_gas(f'prograde_000{i}')
+            sdf, sdf_sinks = sdf_creator(f'{encounter[j]}_000{i}')
+            mappable = subplot_gas(sdf, True)
+
         ax.set_xticks([])
         ax.set_yticks([])
 
