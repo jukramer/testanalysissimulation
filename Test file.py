e83b3acf21e97250bbb41b00bb8389761a986b10
zorkafischer <zorka@dokiten.io>
2026-03-24 15:27:07 +0100

    finslised ax 1

--- Test file.py
+++ Test file.py
@@ -10,24 +10,24 @@ from matplotlib.colors import LogNorm
 SECTIONAL_VIEW = True
 
 sdf, sdf_sinks = sarracen.read_phantom('prograde/prograde_00010')
+
+
 sdf.calc_density()
 
 # sdf.describe()
-#print(sdf)
-#print(sdf_sinks)
+print(sdf)
+print(sdf_sinks)
 # print(sdf['itype'].value_counts())
+
 
 sdf['x'] = sdf['x'] - sdf_sinks.at[0, 'x']
 sdf['y'] = sdf['y'] - sdf_sinks.at[0, 'y']
-sdf['z'] = sdf['z'] - sdf_sinks.at[0, 'z']
 
 sdf_sinks.at[1, 'x'] = sdf_sinks.at[1, 'x'] - sdf_sinks.at[0, 'x'] 
 sdf_sinks.at[1, 'y'] = sdf_sinks.at[1, 'y'] - sdf_sinks.at[0, 'y'] 
-sdf_sinks.at[1, 'z'] = sdf_sinks.at[1, 'z'] - sdf_sinks.at[0, 'z']
 
 sdf_sinks.at[0, 'x'] = sdf_sinks.at[0, 'x'] - sdf_sinks.at[0, 'x']
 sdf_sinks.at[0, 'y'] = sdf_sinks.at[0, 'y'] - sdf_sinks.at[0, 'y'] 
-sdf_sinks.at[0, 'z'] = sdf_sinks.at[0, 'z'] - sdf_sinks.at[0, 'z']
 
 #Creating dots for sink particles
 x_sink_0 = sdf_sinks.at[0, 'x'] 
@@ -38,12 +38,6 @@ y_sink_1 = sdf_sinks.at[1, 'y']
 
 print(sdf)
 print(sdf_sinks)
-
-#Separate into different itypes
-
-sdf_gas = sdf[sdf.itype == 1].copy()
-sdf_dust_1 = sdf[sdf.itype == 7].copy()
-sdf_dust_2 = sdf[sdf.itype == 8].copy()
 
 plt.style.use('dark_background')
 
@@ -58,24 +52,22 @@ def plot_sinks(ax):
     ax.scatter(x=x_sink_0, y=y_sink_0, color='white')
     ax.scatter(x=x_sink_1, y=y_sink_1, color='white')
 
-#
-v_min = 10**(-10)
+
 # function to truncated colour maps
-def truncate_cmap(cmap_name, minval=0.2, maxval=1.0, n=256):
+def truncate_cmap(cmap_name, minval=0.0, maxval=1.0, n=512):
     cmap = plt.get_cmap(cmap_name)
     new_cmap = mcolors.LinearSegmentedColormap.from_list(
         f"trunc_{cmap_name}",
         cmap(np.linspace(minval, maxval, n)))
     new_cmap.set_under(color='black')
     return new_cmap
+cmap_dust = truncate_cmap('Blues_r')
 
-cmap_dust = plt.get_cmap('Blues').copy()
-cmap_dust.set_under('black')
 # sectional view at z = 0 , for sdf.itype, 1 = gas, 7 = dust (stokes = 10), 8 = dust (stokes = 1)
 
 if SECTIONAL_VIEW:
     ax_1 = sdf[sdf.itype == 1].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True, xsec=0.00,
-                                      cmap='Blues_r')
+                                      cmap='bone')
     ax_1.set_title("Gas Distribution in Disc")
     plot_sinks(ax_1)
     plt.show()
@@ -90,10 +82,9 @@ if SECTIONAL_VIEW:
     plot_sinks(ax_3)
     ax_3.set_title("Dust Distribution in Disc (Stokes Number = 1)")
     plt.show()
-    
 else:
     ax_1 = sdf[sdf.itype == 1].render('rho', xlim=(- 400, 400), ylim=(-400, 400), log_scale=True,
-                                      cmap='Blues_r')
+                                      cmap='bone')
     ax_1.set_title("Gas Distribution in Disc")
     plot_sinks(ax_1)
     plt.show()
