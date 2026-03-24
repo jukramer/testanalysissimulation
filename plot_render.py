95b2e94c9cbaa5ccab0ef792ef2b892e0dc4ea8e
zorkafischer <zorka@dokiten.io>
2026-03-24 17:32:39 +0100

    continued plot render

--- plot_render.py
+++ plot_render.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
+import sarracen
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
 from matplotlib.colors import LogNorm
-from Test_file import cmap_dust, sdf_creator, subplot_gas, subplot_dust1, subplot_dust2, plot_sinks
+from Test_file import cmap_dust, sdf_creator, subplot_gas, subplot_dust1, subplot_dust2, plot_sinks, SECTIONAL_VIEW
+
+SECTIONAL_VIEW = True
 
 n_rows = 5
 n_cols = 3
-fig, ax  = plt.subplots(nrows= n_rows, ncols= n_cols)
+fig, axes  = plt.subplots(nrows= n_rows, ncols= n_cols)
 plt.style.use('dark_background')
 mappable = None
 
@@ -14,18 +20,18 @@ norm = LogNorm()
 encounter = ['prograde','retrograde','incl_30']
 for i in range(n_rows):
     for j in range(n_cols):
-        ax = ax[i, j]
+        ax = axes[i,j]
         if i < 10:
-            sdf, sdf_sinks = sdf_creator(f'{encounter[j]}_0000{i}')
+            sdf, sdf_sinks = sdf_creator(f'{encounter[j]}/{encounter[j]}_0000{i}')
             mappable = subplot_gas(sdf, True)
         else:
-            sdf, sdf_sinks = sdf_creator(f'{encounter[j]}_000{i}')
+            sdf, sdf_sinks = sdf_creator(f'{encounter[j]}/{encounter[j]}_000{i}')
             mappable = subplot_gas(sdf, True)
 
         ax.set_xticks([])
         ax.set_yticks([])
 
-cbar = fig.colorbar(mappable=mappable, ax=ax, location='right', fraction = 0.02, pad = 0.02)
+cbar = fig.colorbar(mappable=mappable, ax=axes, location='right', fraction = 0.02, pad = 0.02)
 cbar.set_label("log(rho)")
 plt.tight_layout()
 plt.show()
