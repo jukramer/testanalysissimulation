2e9ad35240a1e8dd4fcf86525989b97dbeb72418
zorkafischer <zorka@dokiten.io>
2026-04-21 09:50:27 +0200

    implenting stuff

--- render_plots.py
+++ render_plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.gridspec import GridSpec
-from render_functions import cmap_dust, sdf_creator, subplot_gas, SECTIONAL_VIEW
+from render_functions import cmap_dust, sdf_creator, subplot_gas, subplot_dust1, subplot_dust2, SECTIONAL_VIEW
 # TODO implement render_functions instead of test_files
 # TODO Clean up python files, delete irrelevant
 # TODO Pick relevant times
@@ -18,39 +18,52 @@ for i in range(n_rows):
 
 cax = fig.add_subplot(gs[:, 3])
 
-mappable_for_cbar = None
+
 encounter = ['prograde', 'retrograde', 'incl_30']
 Time = ['0 yr', '500 yr', '1000 yr', '1500 yr', '2000 yr']
 
-for i in range(n_rows):
-    for j in range(n_cols):
-        ax = axes[i, j]
-        if (i+6)< 10:
-            sdf, sdf_sinks = sdf_creator(f'{encounter[j]}/{encounter[j]}_0000{i+6}')
-        else:
-            sdf, sdf_sinks = sdf_creator(f'{encounter[j]}/{encounter[j]}_000{i+6}')
+def render_plot(subplot, sectional_view, mappable_for_cbar):
+    for i in range(n_rows):
+        for j in range(n_cols):
+            ax = axes[i, j]
+            if (i+6)< 10:
+                sdf, sdf_sinks = sdf_creator(f'{encounter[j]}/{encounter[j]}_0000{i+6}')
+            else:
+                sdf, sdf_sinks = sdf_creator(f'{encounter[j]}/{encounter[j]}_000{i+6}')
 
-        render = subplot_gas(sdf, sdf_sinks, SECTIONAL_VIEW = True , ax = ax, cbar = False)
+            if subplot == "gas":
+                render = subplot_gas(sdf, sdf_sinks, SECTIONAL_VIEW = sectional_view , ax = ax, cbar = False)
+            elif subplot == "dust1":
+                render = subplot_dust1(sdf, sdf_sinks, SECTIONAL_VIEW= sectional_view, ax=ax, cbar=False)
+            elif subplot == "dust2":
+                render = subplot_dust2(sdf, sdf_sinks, SECTIONAL_VIEW= sectional_view, ax=ax, cbar=False)
+            else:
+                print("No render")
+                return None
 
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.set_xlabel('')
-        ax.set_ylabel('')
-        ax.set_aspect('equal')
-        ax.set_xlim(-400, 400)
-        ax.set_ylim(-400, 400)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_xlabel('')
+            ax.set_ylabel('')
+            ax.set_aspect('equal')
+            ax.set_xlim(-400, 400)
+            ax.set_ylim(-400, 400)
 
-        if i == 0:
-            ax.set_title(['Prograde', 'Retrograde', 'Inclined 30°'][j], fontsize=12, pad=10)
+            if i == 0:
+                ax.set_title(['Prograde', 'Retrograde', 'Inclined 30°'][j], fontsize=12, pad=10)
 
-        if j == 0:
-            ax.text(-0.12, 0.5, Time[i], transform=ax.transAxes,
-                    rotation=90, va='center', ha='center', fontsize=10)
+            if j == 0:
+                ax.text(-0.12, 0.5, Time[i], transform=ax.transAxes,
+                        rotation=90, va='center', ha='center', fontsize=10)
 
-        if mappable_for_cbar is None:
-            mappable_for_cbar = render
+            if mappable_for_cbar is None:
+                mappable_for_cbar = render
 
-cbar = fig.colorbar(mappable_for_cbar, cax=cax)
-cbar.set_label("log(rho)")
+    cbar = fig.colorbar(mappable_for_cbar, cax=cax)
+    cbar.set_label("log(rho)")
 
-plt.show()
+    plt.show()
+
+#render_plot("gas", sectional_view = True, mappable_for_cbar = None)
+render_plot("dust1", sectional_view = True, mappable_for_cbar = None)
+#render_plot("dust2", sectional_view = True, mappable_for_cbar = None)
