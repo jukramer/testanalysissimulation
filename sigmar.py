5fec77c6837fa1476d4a0e50e28191129a259404
jukramer <justas.kramer@gmail.com>
2026-04-28 10:26:34 +0200

    dust to gas ratio

--- sigmar.py
+++ sigmar.py
@@ -8,9 +8,13 @@ MASS_GAS = 4e-8
 MASS_DUST_1 = 2e-9
 MASS_DUST_2 = 2e-9
 
-
 def loadData(filepath):
     sdfGas, sdfDust1, sdfDust2, sdf_sinks = sarracen.read_phantom(filepath, separate_types='all')
+    
+    print(sdfDust1['dustfrac'])
+    print(sdfDust2['dustfrac'])
+    print(sdfDust1['dustfrac_2'])
+    print(sdfDust2['dustfrac_2'])
     
     global sdfSinks0
     sdfSinks0 = sdf_sinks.copy()
@@ -23,6 +27,10 @@ def loadData(filepath):
     sdfDust1['mass'] = MASS_DUST_1
     sdfDust2['mass'] = MASS_DUST_2
     
+    print(sdfGas.keys())
+    print(sdfDust1.keys())
+    print(sdfDust2.keys())
+    
     # Interpolation
     gaslocations = np.column_stack([sdfGas['x'], sdfGas['y'], sdfGas['z']])
     interp = NearestNDInterpolator(gaslocations, sdfGas['rho'] )
@@ -30,6 +38,15 @@ def loadData(filepath):
     sdfDust1['interpDustDensity'] = interp(dustlocations1)
     dustlocations2 = np.column_stack([sdfDust2['x'], sdfDust2['y'], sdfDust2['z']])
     sdfDust2['interpDustDensity'] = interp(dustlocations2)
+    
+    # Dust-to-gas ratio
+    dustfrac1Vals = sdfDust1['dustfrac'].to_numpy()
+    sdfDust1['dust-to-gas'] = dustfrac1Vals/(1-dustfrac1Vals)
+    dustfrac2Vals = sdfDust2['dustfrac'].to_numpy()
+    sdfDust2['dust-to-gas'] = dustfrac2Vals/(1-dustfrac2Vals)
+    
+    # print(sdfDust1['dust-to-gas'])
+    # print(sdfDust2['dust-to-gas'])
     
     return sdfGas, sdfDust1, sdfDust2, sdf_sinks
 
@@ -75,7 +92,7 @@ def calcSigma(sdf, n, rIn, rOut):
     
     return rVals[:-1], sigmaVals
 
-
+       
 def azimuthBin(sdf, col, nBins):
     azimuthBins = np.linspace(-np.pi, np.pi, nBins + 1, True)
     partIDs = []
@@ -118,20 +135,20 @@ def trackPart(orbitType, cols, dustType, nSnapshots=13, nAzimuthBins=50, avg=Tru
         except FileNotFoundError:
             dustArray = np.delete(dustArray, i, 2)
             
-    print(dustArray)
     if avg:
         # averages amonst all particles
         return np.mean(dustArray, 1, keepdims=True)
     else:
         return dustArray
-            
-            
+
+
 if __name__ == '__main__':
     nSnaps = 13
-    meanValsArr = trackPart('prograde', ['interpDustDensity'], 1, nSnapshots=nSnaps)
-    print(meanValsArr)
+    meanValsArr = trackPart('prograde', ['dustfrac'], 1, nSnapshots=nSnaps)
     
+    ### PLOTTING ###
     tVals = np.linspace(0, 1, nSnaps)[:,np.newaxis]
     plt.plot(tVals, meanValsArr[0,:,:].T)
     plt.show()
-
+    
+    
